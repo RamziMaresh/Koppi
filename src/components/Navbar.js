388f16f073fdf0727fd57338ff89616255e2b031
RAMZI MARESH AL-FAIZE <47695617+RamziMaresh@users.noreply.github.com>
2022-03-19 01:40:11 +0800

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -29,7 +29,7 @@ function Navbar() {
       <nav className='navbar'>
         <div className='navbar-container'>
           <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
-          <img src='/Koppi/public/images/kopii-logo22.png'></img>
+          <img src="Koppi/public/images/kopii-logo22.png'/>
           </Link>
           <div className='menu-icon' onClick={handleClick}>
             <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
